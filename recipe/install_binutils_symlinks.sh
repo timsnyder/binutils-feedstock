f8babf816137fe5ddbbbf4986737c43583b0bd54
Isuru Fernando <isuruf@gmail.com>
2019-10-27 05:58:45 +0000

    remove skip again

--- recipe/install_binutils_symlinks.sh
+++ recipe/install_binutils_symlinks.sh
@@ -3,6 +3,8 @@
 CHOST="${ctng_cpu_arch}-${ctng_vendor}-linux-gnu"
 
 for tool in addr2line ar as c++filt dwp elfedit gprof ld ld.bfd ld.gold nm objcopy objdump ranlib readelf size strings strip; do
+  rm $PREFIX/bin/$CHOST-$tool
+  touch $PREFIX/bin/$CHOST-$tool
   ln -s $PREFIX/bin/$CHOST-$tool $PREFIX/bin/$tool
 done
 
