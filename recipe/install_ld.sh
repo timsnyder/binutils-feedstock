e90dae653aa0260cdb6923cfae82c571f4d8379e
Isuru Fernando <isuruf@gmail.com>
2019-11-21 23:19:12 +0000

    No binary relocation

--- recipe/install_ld.sh
+++ recipe/install_ld.sh
@@ -6,5 +6,6 @@ cd build
 
 make DESTDIR=$PWD/install install-strip
 
+CHOST="${ctng_cpu_arch}-${ctng_vendor}-linux-gnu"
 mkdir -p $PREFIX/bin
-cp $PWD/install/$PREFIX/bin/$HOST-ld $PREFIX/bin/$HOST-ld
+cp $PWD/install/$PREFIX/bin/$CHOST-ld $PREFIX/bin/$CHOST-ld
