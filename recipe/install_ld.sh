c95f7140dd6de4f15a10791d0b9fd147dda1614e
Matthew R Becker <becker.mr@gmail.com>
2020-07-10 13:01:52 -0500

    BUG add sysroot symlinks

--- recipe/install_ld.sh
+++ recipe/install_ld.sh
@@ -7,5 +7,11 @@ cd build
 make DESTDIR=$PWD/install install-strip
 
 CHOST="${ctng_cpu_arch}-${ctng_vendor}-linux-gnu"
+NEW_CHOST="${ctng_cpu_arch}-conda-linux-gnu"
 mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/$NEW_CHOST/bin
+mkdir -p $PREFIX/$CHOST/bin
 cp $PWD/install/$PREFIX/bin/$CHOST-ld $PREFIX/bin/$CHOST-ld
+ln -s $PREFIX/bin/$CHOST-ld $PREFIX/bin/$NEW_CHOST-ld
+ln -s $PREFIX/bin/$CHOST-ld $PREFIX/$NEW_CHOST/bin/ld
+ln -s $PREFIX/bin/$CHOST-ld $PREFIX/$CHOST/bin/ld
