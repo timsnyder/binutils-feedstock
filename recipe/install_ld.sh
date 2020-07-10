8768576187003066b5610f92bd1e37ec2c6ec89c
Matthew R Becker <beckermr@users.noreply.github.com>
2020-07-10 15:24:37 -0500

    Update recipe/install_ld.sh
    Co-authored-by: Isuru Fernando <isuruf@gmail.com>

--- recipe/install_ld.sh
+++ recipe/install_ld.sh
@@ -9,7 +9,5 @@ make DESTDIR=$PWD/install install-strip
 CHOST="${ctng_cpu_arch}-${ctng_vendor}-linux-gnu"
 NEW_CHOST="${ctng_cpu_arch}-conda-linux-gnu"
 mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/$NEW_CHOST/bin
-mkdir -p $PREFIX/$CHOST/bin
 cp $PWD/install/$PREFIX/bin/$CHOST-ld $PREFIX/bin/$CHOST-ld
 ln -s $PREFIX/bin/$CHOST-ld $PREFIX/bin/$NEW_CHOST-ld
