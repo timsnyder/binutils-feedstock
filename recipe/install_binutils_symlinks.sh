4ae656262b7ffc54cf1e10cba2662f0ee19976c2
Tim Snyder <snyder.tim@gmail.com>
2022-03-25 20:42:11 +0000

    inject rpath in short ld commands
    The short symlinks can be utilized without the activation scripts. A use case
    where this might be helpful is if one was cross compiling and thus the activation
    scripts would be primarily concerned with the HOST or TARGET architecture.  Standards
    for FLAGS that apply to the BUILD are a bit varied  FLAGS_FOR_BUILD is used in some
    cases but there are others that completely ignore those.
    
    The symlinked short-name tools require CONDA_PREFIX/include and the CONDA_PREFIX/lib rpath
    at a minimum.  For tools that need those flags, inject them in small wrapper scripts.
    They are installed with the PREFIX and thus will be modified at install time to have the
    installed PREFIX hardcoded.
    
    In the case of binutils, we have the additional advantage when using wrapper scripts
    that we can tweak LDFLAGS to the idiosyncrasies of each ld implementation.  Of note,
    LDFLAGS are typically passed into the gcc driver.  However, there are numerous build
    scripts that call ld directly with LDFLAGS and having the '-Wl' (send to the linker)
    option for the gcc driver causes annoying errors.  Also, ld.gold doesn't understand
    -rpath-link, it expects --rpath-link.

--- recipe/install_binutils_symlinks.sh
+++ recipe/install_binutils_symlinks.sh
@@ -2,10 +2,45 @@
 
 CHOST="${ctng_cpu_arch}-conda-linux-gnu"
 
-for tool in addr2line ar as c++filt dwp elfedit gprof ld ld.bfd ld.gold nm objcopy objdump ranlib readelf size strings strip; do
+# The minimal set of flags that need to be applied for these short names to just do what I want
+# are:
+#LDFLAGS=(-Wl,-rpath,"${PREFIX}/lib" -Wl,-rpath-link,"${PREFIX}/lib" -L"${PREFIX}/lib") 
+# the standard way we set LDFLAGS works when the makefiles call gcc driver but not if they
+# call ld directly becasue -Wl is the driver option, not linker option
+LDFLAGS=(-rpath "${PREFIX}/lib" -rpath-link "${PREFIX}/lib" -L"${PREFIX}/lib") 
+
+# So, for the commands that need those flags, install tiny wrapper scripts that inject them
+
+wrapit() {
+    long=$1
+    shift
+    short=$1
+    shift
+
+    # NOTE the importance of tab characters in the <<- HEREDOC, i.e. they will be dedented
+	cat > $short <<-END_WRAP
+	#!${PREFIX}/bin/bash
+
+	exec $long $@ "\$@"
+	END_WRAP
+}
+
+for tool in addr2line ar as c++filt dwp elfedit gprof nm objcopy objdump ranlib readelf size strings strip; do
   rm $PREFIX/bin/$CHOST-$tool
   touch $PREFIX/bin/$CHOST-$tool
   ln -s $PREFIX/bin/$CHOST-$tool $PREFIX/bin/$tool
+done
+for tool in ld ld.bfd ld.gold; do
+  rm $PREFIX/bin/$CHOST-$tool
+  touch $PREFIX/bin/$CHOST-$tool
+  wrapit $PREFIX/bin/$CHOST-$tool $PREFIX/bin/$tool "${LDFLAGS[@]}"
+done
+# Thanks for being special ld.gold.  It doesn't understand -rpath-link, it needs an extra '-' on the front of it, i.e. --rpath-link
+LDFLAGS=${LDFLAGS[@]/-rpath-link/--rpath-link}
+for tool in ld.gold; do
+  rm $PREFIX/bin/$CHOST-$tool
+  touch $PREFIX/bin/$CHOST-$tool
+  wrapit $PREFIX/bin/$CHOST-$tool $PREFIX/bin/$tool "${LDFLAGS[@]}"
 done
 
 ln -s "$PREFIX/bin/ld.gold" "$PREFIX/bin/gold"
